405ba5bfbd2535817ca17a1a4ff421771ce8a858
achikovar <116868666+achikovar@users.noreply.github.com>
2024-03-21 16:21:43 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+// 1
+
+// let array = [0,1,2,3,4,5]
+
+// console.log(array.length)
+
+// 2
+
+// let arr= ['a', 'b', 'c']
+
+// arr.push('d')
+
+// arr[1] = 'x'
+
+// console.log(arr)
+
+// 3
+
+// arr = []
+
+// arr.push('a','b','c')
+
+// console.log(arr)
+
+
+// 4
+
+// let arr= [1, 2, 3, 4, 5];
+// let key1= arr[0]
+// let key2= arr[1]
+
+// result = key1+key2
+
+// console.log(result)
+
+
+// 5
+
+// let arr= ['a', 'b', 'c', 'd', 'e'];
+
+// arr.splice(1,1)
+// arr.splice(2,1)
+
+// console.log(arr)
+
+
+// 6
+
+// let names= ['ხვიჩა', 'გოგა','მაკა', 'ანა', 'ინა']
+
+// names.sort()
+
+// console.log(names);
+
+// names.reverse()
+
+// console.log(names)
+
+
+// 7
+
+// let arr= [11, 22, 33, 44, 55];
+// console.log(arr.length);
+
+
+// 8
+
+// let arr= [10, 11, 12, 13, 14];
+// console.log(arr.length);
+
+
+// 9
+
+// let arr= [10, 11, 12, 13, 14];
+// console.log(arr.length);
+
+
+// 10
+
+// const getBlankarray = (num) => num = []
+
+// console.log(getBlankarray())
+
+
+// 11
+
+// let getNumOfComponents = components  => components = [1,2,3]
+
+// console.log(getNumOfComponents().length)
+
+
+// 12
+
+// console.log(
+//     priceSum = [10, 20, 3, 4].reduce((a, b) => a + b)
+//   )
+
+
+// 13
+
+// arr = ['ოვერბორდი', 'ფაზლი', 'ლეგო']
+// console.log(arr)
+
+// let addBall = arrAdd => arrAdd = arr.push('ბურთი')
+
+// addBall()
+
+// console.log(arr)
+
+
+// 14
+ 
+// let toys = ['ოვერბორდი', 'პაზლი', 'Lego']
+
+// function myFoo (toys,toy) {
+//     toy = 'Yo-yo';
+//     toys.push(toy)
+//     return toys
+// }
+
+// console.log(myFoo(toys))
+
+
+// 15
+
+// let toys= ['ოვერბორდი', 'პაზლი', 'Lego', 'Yo-yo']
+
+// let getLasttoy = last => last = toys[toys.length - 1];
+
+// console.log(getLasttoy())
+
+
+
